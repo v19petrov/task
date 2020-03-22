86d1640b83bf39eef0cbfaa8591012d0096aa408
v19petrov <62064954+v19petrov@users.noreply.github.com>
2020-03-22 17:23:28 +0300

    Delete task_1.py

--- task_1.py
+++
@@ -1,9 +0,0 @@
-name = 'Vladimir'
-age = 43
-toun = 'Moscow'
-print('меня зовут', name, 'мне', age, 'года', 'проживаю в городе', toun)
-
-user_name = input('Введите ваше имя: ')
-user_age = int(input('Сколько вам лет?: '))
-residence = input('Гда вы проживаете?: ')
-print('Дайте угадаю...Вас зовут', user_name, ', правильно?', 'Вам', user_age, 'года и вы проживаете в городе', residence)
