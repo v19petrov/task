0f8d2335aee075a399c5cf4d75ae0eec03e68ef5
v19petrov <62064954+v19petrov@users.noreply.github.com>
2020-03-22 17:24:37 +0300

    Delete task_3.py

--- task_3.py
+++
@@ -1,9 +0,0 @@
-number = int(input('Ведите число: '))
-if number >= 100:
-    number = int(input('Введите двузначное число: '))
-if number < 0:
-    number = int(input('Введите положительное двузначное число: '))
-number_2 = number*11
-number_3 = number*111
-
-print((str(number) + '+' + str(number_2) + '+' + str(number_3)), '=', (number + number_2 + number_3))
