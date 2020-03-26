2f2681a3bd597eb1257d439c19570c6017966bfe
v19petrov <62064954+v19petrov@users.noreply.github.com>
2020-03-26 15:18:11 +0300

    Delete task2_2.py

--- task2_2.py
+++
@@ -1,9 +0,0 @@
-user_list = list(input('Enter a number from a set of digits: '))
-print(user_list)
-
-j = 0
-for i in range(int(len(user_list) / 2)):
-    user_list[j], user_list[j + 1] = user_list[j + 1], user_list[j]
-    j += 2
-
-print(user_list)
