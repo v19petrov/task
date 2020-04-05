4baac0ee55c683a424e6f6a7ce4cfac6a75ec410
v19petrov <62064954+v19petrov@users.noreply.github.com>
2020-04-05 19:36:48 +0300

    Delete task4.py

--- task4.py
+++
@@ -1,7 +0,0 @@
-def payroll():
-    name = input('Имя сотрудника: ')
-    rate_per_hour = int(input('Ставка в час: '))
-    working_hours = int(input('Количество часов: '))
-    prize = int(input('Премия: '))
-    return (f'{name} получает {rate_per_hour*working_hours} печенек и премию {prize} печенек. Всего: {rate_per_hour*working_hours + prize} печенек')
-
