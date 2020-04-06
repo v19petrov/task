9261f388b0c38bc5c561e387cf3c1db26112e67c
v19petrov <62064954+v19petrov@users.noreply.github.com>
2020-04-06 16:27:21 +0300

    Delete task5_2_2.py

--- task5_2_2.py
+++
@@ -1,8 +0,0 @@
-with open('task5_2_1.txt', 'r') as my_f:
-    my_l = my_f.readlines()
-    for index, value in enumerate(my_l):
-        num = len(value.split())
-        print('Number of words in line: {}'.format(num))
-with open('task5_2_1.txt', 'r') as my_f:
-    count = sum(1 for line in my_f)
-    print('Number of lines in the file: {}'.format(count))
